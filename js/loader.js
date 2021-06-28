f63b860b78fe447e7bf9851e18e1e8aebcbfa6bc
codebyleon <leonkoschorreck@Leons-MacBook-Pro.fritz.box>
2021-06-28 10:39:00 +0200

    added image behaviour

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,13 @@
 const loader = document.querySelector('.loader');
 const main = document.querySelector('.main');
+const sol = document.querySelector('.sol');
 
 function init() {
     setTimeout(() => {
         loader.style.opacity = 0;
         loader.style.display = 'none';
+        sol.style.opacity = 0;
+        sol.style.display = 'none';
 
         main.style.display = 'block';
         setTimeout(() => main.style.opacity = 1, 50);
